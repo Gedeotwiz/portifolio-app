f3dccd4e0c717846c22260ba6500df9d0cb93080
Twizerimana <gedeontwizerimana6@gmail.com>
2026-07-14 17:58:15 +0300

    object

--- js/cutchup.js
+++ js/cutchup.js
@@ -9,5 +9,36 @@ const word = "Hello word"
 console.log(word)
 
 const numbers = [1,2,3,4,5]
-const student = ["kamana","kamali","kalisa","kamikaza"]
+const student = ["kamana","kamali","kalisa","kamikaza",5,]
+console.log(typeof student)
+
+
+//example of data type
+
+// 1. string : It is all words even if it is small letter or capital letter 
+
+const name = "Kalisa"|| 'Kalisa'
+console.log(typeof name ,name)
+
+const numeric = 7
+console.log(typeof numeric ,numeric)
+
+const isActive = true
+console.log(typeof isActive , isActive)
+
+const toDay = new Date
+console.log(typeof toDay , toDay)
+
+
+// Object
+
+const book = {
+    title:"English",
+    publishedAt:2005,
+    price:5000
+}
+console.log(typeof book, book)
+
+
+
  
